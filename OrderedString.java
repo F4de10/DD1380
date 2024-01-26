37d8657848c942af07bf1edde5fede45f909a123
F4de10 <149432824+F4de10@users.noreply.github.com>
2024-01-26 11:43:48 +0100

    Refactor main method to calculate the length of the longest sorted substring

--- OrderedString.java
+++ OrderedString.java
@@ -1,22 +1,28 @@
 import java.util.Scanner;
 
 public class OrderedString {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        int currentLength = 1;
 
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) >= s.charAt(i - 1)) {
-                currentLength++;
-            } else {
-                currentLength = 1;
-            }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String s = sc.nextLine();
+    int maxLength = 1;
+    int currentLength = 1;
+
+    for (int i = 1; i < s.length(); i++) {
+      if (s.charAt(i) >= s.charAt(i - 1)) {
+        currentLength++;
+      } else {
+        if (currentLength > maxLength) {
+          maxLength = currentLength;
         }
-
-        System.out.println("Length of the shortest sorted substring: " + currentLength);
+        currentLength = 1;
+      }
     }
+
+    if (currentLength > maxLength) {
+      maxLength = currentLength;
+    }
+
+    System.out.println("Length of the longest sorted substring: " + maxLength);
+  }
 }
-
-
-
