ed8bf76d62025c2a9c9ae8ab0e84f3af564f1521
F4de10 <149432824+F4de10@users.noreply.github.com>
2024-01-26 18:24:29 +0100

    Refactor code to improve readability and handle whitespace characters

--- OrderedString.java
+++ OrderedString.java
@@ -2,26 +2,29 @@ import java.util.Scanner;
 
 public class OrderedString {
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    String s = sc.nextLine();
-    int maxLength = 1;
-    int currentLength = 1;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
 
-    for (int i = 1; i < s.length(); i++) {
-      if (s.charAt(i) > s.charAt(i - 1)) {
-        currentLength++;
-      } else {
-        if (currentLength > maxLength) {
-          maxLength = currentLength;
-        }
-        if (s.charAt(i) <= s.charAt(i - 1)) {
-          currentLength = 1;
-        }
-      }
-    }
+		int maxLength = 1;
+		int currentLength = 1;
+		String order = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    sc.close();
-    System.out.println(maxLength);
-  }
+		for (int i = 1; i < s.length(); i++) {
+			if (Character.isWhitespace(s.charAt(i))) {
+				currentLength = 0;
+				continue;
+			}
+			if (order.indexOf(s.charAt(i)) > order.indexOf(s.charAt(i - 1))) {
+				currentLength++;
+				if (currentLength > maxLength) {
+					maxLength = currentLength;
+				}
+			} else {
+				currentLength = 1;
+			}
+		}
+		sc.close();
+		System.out.println(maxLength);
+	}
 }
