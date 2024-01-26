94b958ca75dce1d91ff0589d03153b376f44c493
F4de10 <149432824+F4de10@users.noreply.github.com>
2024-01-26 11:27:30 +0100

    Refactor code: Update access modifier and add input validation in OrderedString.java

--- src/OrderedString.java
+++ src/OrderedString.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class OrderedString {
+import java.util.Scanner;
+
+public class OrderedString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int currentLength = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) >= s.charAt(i - 1)) {
+                currentLength++;
+            } else {
+                currentLength = 1;
+            }
+        }
+
+        System.out.println("Length of the shortest sorted substring: " + currentLength);
+    }
 }
+
+
+
